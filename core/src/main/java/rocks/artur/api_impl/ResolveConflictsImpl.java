5892d54f568f128616ff1cb026c7be5d52b72d4f
artur <me@artur.rocks>
2023-12-12 15:20:35 +0100

    Adding conf res

--- core/src/main/java/rocks/artur/api_impl/ResolveConflictsImpl.java
+++ core/src/main/java/rocks/artur/api_impl/ResolveConflictsImpl.java
@@ -25,6 +25,50 @@ public class ResolveConflictsImpl implements ResolveConflicts {
     }
 
     private void updateWeights() {
+        Map<String, Double> score = sources.stream().collect(Collectors.toMap(
+                Function.identity(),
+                s -> 0.0));
+
+        Map<String, Double> count = sources.stream().collect(Collectors.toMap(
+                Function.identity(),
+                s -> 0.0));
+
+
+        List<String[]> entries = characterisationResultGateway.getFilepathProperty();
+
+        for (String[] entry : entries) {
+            List<CharacterisationResult> characterisationResults = characterisationResultGateway.getCharacterisationResultsByFilepathProperty(entry[0], Property.valueOf(entry[1]));
+
+            for (CharacterisationResult characterisationResult : characterisationResults) {
+
+                String trueValue = truth.get(entry);
+
+                String value = characterisationResult.getValue();
+                String source = characterisationResult.getSource();
+                if (trueValue.equals(value)) {
+                    score.put(source, score.getOrDefault(source, 0.0) + 0);
+                } else {
+                    score.put(source, score.getOrDefault(source, 0.0) + 1);
+                }
+                count.put(source, count.getOrDefault(source, 0.0) + 1);
+            }
+        }
+        for (String source : score.keySet()) {
+            Double countSource = count.getOrDefault(source, 1.0);
+            score.put(source, score.get(source) / countSource);
+        }
+        Double sum = score.values().stream().reduce(0.0, (a, b) -> a + b);
+
+        score.replaceAll((s, v) -> score.get(s) / sum);
+
+        Optional<Map.Entry<String, Double>> max = score.entrySet().stream().max(Map.Entry.comparingByValue());
+        if (max.isPresent()) {
+            Double norm_score = max.get().getValue();
+            for (String source : score.keySet()) {
+                double w = score.get(source) / norm_score;
+                sourceWeights.put(source, -Math.log(w) + 0.00001);
+            }
+        }
     }
 
     private void generateTruth() {
@@ -33,7 +77,7 @@ public class ResolveConflictsImpl implements ResolveConflicts {
 
     List<String> sources;
     Map<String, Double> sourceWeights;
-    Map<CharacterisationResult, String> truth;
+    Map<String[], String> truth;
 
     void getData() {
 
@@ -59,17 +103,13 @@ public class ResolveConflictsImpl implements ResolveConflicts {
                     String source = characterisationResult.getSource();
                     Double sourceWeight = sourceWeights.get(source);
                     String value = characterisationResult.getValue();
-                    if (votingScores.containsKey(value)) {
-                        Double score = votingScores.get(value);
-                        votingScores.put(value, score + sourceWeight);
-                    } else {
-                        votingScores.put(value, sourceWeight);
-                    }
+
+                    votingScores.put(value, votingScores.getOrDefault(value, 0.0) + sourceWeight);
                 }
                 Optional<Map.Entry<String, Double>> first = votingScores.entrySet().stream().max(Map.Entry.comparingByValue());
                 if (first.isPresent()) {
                     String trueValue = first.get().getKey();
-                    truth.put(firstResult, trueValue);
+                    truth.put(entry, trueValue);
                 }
 
 

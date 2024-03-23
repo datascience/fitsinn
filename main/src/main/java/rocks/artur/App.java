f478d97e585a6888d0af65c931aace9233b38add
artur <me@artur.rocks>
2024-03-23 23:43:14 +0100

    Stabilizing the code

--- main/src/main/java/rocks/artur/App.java
+++ main/src/main/java/rocks/artur/App.java
@@ -1,8 +1,5 @@
 package rocks.artur;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.SpringApplication;
@@ -11,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
-import org.springframework.core.type.filter.TypeFilter;
 
 import java.util.Arrays;
 
@@ -27,40 +23,26 @@ public class App {
     BeanFactoryPostProcessor beanFactoryPostProcessor() {
         return beanFactory -> genericApplicationContext((BeanDefinitionRegistry) beanFactory);
     }
-
     void genericApplicationContext(BeanDefinitionRegistry beanRegistry) {
         ClassPathBeanDefinitionScanner beanDefinitionScanner = new ClassPathBeanDefinitionScanner(beanRegistry);
         String profile = System.getenv("DB_SELECTOR") == null ? System.getProperty("spring.profiles.active", "h2") : System.getenv("DB_SELECTOR");
         System.out.println(profile);
-        beanDefinitionScanner.addIncludeFilter(removeModelAndEntitiesFilter());
+        beanDefinitionScanner.addIncludeFilter(App::match);
         String[] packages;
         switch (profile) {
-            case "clickhouse":
-                packages = new String[]{"rocks.artur.api", "rocks.artur.api_impl", "rocks.artur.FITSClient", "rocks.artur.endpoints.RestService", "rocks.artur.clickhouse"};
-                break;
-            case "h2":
-            case "mysql":
-                packages = new String[]{"rocks.artur.api", "rocks.artur.api_impl", "rocks.artur.FITSClient", "rocks.artur.endpoints.RestService", "rocks.artur.jpa"};
-                break;
-            default:
-                throw new UnsupportedOperationException("The selected db is not supported. Choose one from [clickhouse, mysql, h2]");
+            case "clickhouse" ->
+                    packages = new String[]{"rocks.artur.api", "rocks.artur.api_impl", "rocks.artur.FITSClient", "rocks.artur.endpoints.RestService", "rocks.artur.clickhouse"};
+            case "h2", "mysql" ->
+                    packages = new String[]{"rocks.artur.api", "rocks.artur.api_impl", "rocks.artur.FITSClient", "rocks.artur.endpoints.RestService", "rocks.artur.jpa"};
+            default ->
+                    throw new UnsupportedOperationException("The selected db is not supported. Choose one from [clickhouse, mysql, h2]");
         }
         beanDefinitionScanner.scan(packages);
     }
-
-    TypeFilter removeModelAndEntitiesFilter() {
-        return (MetadataReader mr, MetadataReaderFactory mrf) -> {
-            System.out.println(mr.getClassMetadata()
-                    .getClassName());
-            return !mr.getClassMetadata()
-                    .getClassName()
-                    .startsWith("rocks.artur.domain") &&
-                    !mr.getClassMetadata()
-                            .getClassName()
-                            .startsWith("rocks.artur.api_impl.filter") &&
-                    !mr.getClassMetadata()
-                            .getClassName()
-                            .startsWith("rocks.artur.api_impl.utils");
-            };
+    private static boolean match(MetadataReader mr, MetadataReaderFactory mrf) {
+        String className = mr.getClassMetadata().getClassName();
+        System.out.println(className);
+        String[] packagesToIgnore = new String[]{"rocks.artur.domain", "rocks.artur.api_impl.filter", "rocks.artur.api_impl.utils"};
+        return Arrays.stream(packagesToIgnore).noneMatch(className::startsWith);
     }
 }

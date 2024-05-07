6bfa4ce075982edea406a74b228466bca60ea53c
artur <me@artur.rocks>
2024-05-08 00:41:41 +0200

    Add dropdown component

--- web/frontend/src/scenes/global/Topbar.jsx
+++ web/frontend/src/scenes/global/Topbar.jsx
@@ -1,5 +1,9 @@
-import { Box, IconButton, useTheme } from "@mui/material";
-import { useContext } from "react";
+import {Box, Button, IconButton, Typography, useTheme} from "@mui/material";
+import React, { useContext } from "react";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 import { ColorModeContext, tokens } from "../../theme";
 
@@ -9,9 +13,20 @@ const Topbar = () => {
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
   return (
-    <Box m="20px">
+    <Box  m="20px"  display="flex" justifyContent="space-between" >
       <Filter />
+      <Box   >
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel>Dataset</InputLabel>
+        <Select label="Dataset">
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      </Box>
     </Box>
+
   );
 };
 

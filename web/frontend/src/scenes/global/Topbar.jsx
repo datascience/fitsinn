93ce7175301cb446a052c737c7b304143c406da8
artur <artur@artur.artur>
2024-05-09 22:41:54 +0200

    adding ui components

--- web/frontend/src/scenes/global/Topbar.jsx
+++ web/frontend/src/scenes/global/Topbar.jsx
@@ -1,32 +1,36 @@
 import {Box, Button, IconButton, Typography, useTheme} from "@mui/material";
-import React, { useContext } from "react";
+import React, {useContext, useState} from "react";
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-
+import TextField from '@mui/material/TextField';
 import { ColorModeContext, tokens } from "../../theme";
 
 import Filter from "../../components/Filter";
 const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+
+  const [datasets, setDatasets] = useState([]);
+
   const colorMode = useContext(ColorModeContext);
   return (
-    <Box  m="20px"  display="flex" justifyContent="space-between" >
-      <Filter />
-      <Box   >
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel>Dataset</InputLabel>
-        <Select label="Dataset">
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
+      <Box  m="20px" display="flex" justifyContent="space-between">
+        <Box>
+          <Filter />
+        </Box>
+        <Box>
+          <FormControl sx={{minWidth: 120 }}>
+            <InputLabel>Dataset</InputLabel>
+            <Select label="Dataset">
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
       </Box>
-    </Box>
-
   );
 };
 
--- web/frontend/src/scenes/uploadForm/index.jsx
+++ web/frontend/src/scenes/uploadForm/index.jsx
@@ -1,14 +1,40 @@
-import { Box } from "@mui/material";
+import {Box, Button} from "@mui/material";
 import Header from "../../components/Header";
 import Upload from "../../components/Upload";
+import TextField from "@mui/material/TextField";
+import React from "react";
+import {useContext, useState} from "react";
+
 
 const UploadForm = () => {
+   const createDataset = (name) => {
+       console.log(textInput);
+   }
+
+    const [textInput, setTextInput] = useState('');
+
+    const handleTextInputChange = event => {
+        setTextInput(event.target.value);
+    };
+
+
+
+
   return (
     <Box m="20px">
       <Header
         title="Add Your Data"
         subtitle="Here you can upload you collection"
       ></Header>
+
+        <Box  marginBottom="20px" display="flex" justifyContent="left">
+            <TextField id="outlined-basic" label="Name"  value= {textInput}
+                       onChange= {handleTextInputChange}/>
+            <Button onClick={createDataset} color="secondary" variant="contained">
+                Create Dataset
+            </Button>
+        </Box>
+
       <Box height="75vh">
         <Upload />
       </Box>
